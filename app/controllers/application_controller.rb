c5ca85e47777f0e317948cc33a5e23d9237514f4
Laura <laura@laura-lenovo.(none)>
2013-02-16 22:52:40 -0600

    301 redirect to www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'www.lauradhamilton.com'
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN and ENV['RAILS_ENV'] == 'production'
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
+
   protect_from_forgery
 
   private
